90f922aaf446648a1880b000d6b4de1408acf8ee
muhammedMoussa <muhammedmoussa@hotmail.com>
2020-11-25 01:09:53 +0200

    added: shared loading component

--- src/app/shared/shared/shared.module.ts
+++ src/app/shared/shared/shared.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoaderComponent } from './components/loader/loader.component';
 
 const SHARED_MODULES = [CommonModule];
 
+const SHARED_COMPONENTS = [LoaderComponent]
 @NgModule({
-  declarations: [],
+  declarations: [...SHARED_COMPONENTS],
   imports: [
     ...SHARED_MODULES
   ],
   exports: [
-    ...SHARED_MODULES
+    ...SHARED_MODULES,
+    ...SHARED_COMPONENTS
   ]
 })
 export class SharedModule { }
